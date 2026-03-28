fea97ee799534c45cfd7d09a7f65182ecd1a567b
davis stermer <davisstermer@gmail.com>
2026-03-28 01:32:24 -0400

    fixed

--- lua/plugins/blinkcmp.lua
+++ lua/plugins/blinkcmp.lua
@@ -48,6 +48,13 @@ return {
       nerd_font_variant = 'mono'
     },
 
+    completion = {
+
+        menu = {auto_show = false},
+        ghost_text = {enabled = true},
+    },
+    
+
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
